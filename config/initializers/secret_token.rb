e3ea3dd63af5a3f342b52c2df930dad986f9286e
leslie bienenfeld <lbienenfeld@gmail.com>
2014-02-16 21:15:20 -0700

    create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UDCPortfolio2::Application.config.secret_token = 'e337fc226ab310b77cca2c25fcfd6b4f5f7563da30a82928d37d27e920cfd340adca97d155c312f905a25883096b1104730fa5166a274063e85409f541de89cb'
